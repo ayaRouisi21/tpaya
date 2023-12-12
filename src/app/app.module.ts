ff9459f67ce4624e1a32d68f605c2929cfc24d0e
ayaRouisi21 <ayoutarouissi.new@gmail.com>
2023-12-12 10:17:55 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { Form1Component } from './form1/form1.component';
+import { SignupComponent } from './signup/signup.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    Form1Component,
+    SignupComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/image/image.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.scss']
+})
+export class ImageComponent {
+   imageUrl: string = '';
+} 
+
+
+
